c9b2bc9bb50d1b26bd3e2796b2d3597ad0b8fad5
RedArms <47068819+RedArms@users.noreply.github.com>
2023-09-15 08:42:19 +0200

    config eslint

--- exercices/module1/part1/src/App.jsx
+++ exercices/module1/part1/src/App.jsx
@@ -3,11 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function Helloworld() {
+  return <h1>Hello World</h1>
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Helloworld />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
